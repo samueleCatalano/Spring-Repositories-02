490f1346d1cd25152cd5359dbb4ce124136574d9
Samuele Catalano <csamuele73@mail.com>
2022-10-19 19:52:16 +0200

    first commit

---
+++ src/main/java/com/springrepositories02/springrepositories02/Springrepositories02Application.java
@@ -0,0 +1,13 @@
+package com.springrepositories02.springrepositories02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springrepositories02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springrepositories02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springrepositories02/springrepositories02/Springrepositories02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springrepositories02.springrepositories02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springrepositories02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
